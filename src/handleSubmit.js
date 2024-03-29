773df7d6a2a7add698f375f5a64acd5e019ac016
trinm-3423 <nguyen.minh.tri@sun-asterisk.com>
2024-03-29 17:06:53 +0700

    Add eslint and prettier

--- src/handleSubmit.js
+++ src/handleSubmit.js
@@ -1,43 +1,48 @@
-import puppeteer from "puppeteer";
-import measureExecutionTime from "./utils/measureExecutionTime.js";
+import puppeteer from 'puppeteer';
+import measureExecutionTime from './utils/measureExecutionTime.js';
 
 const handleSubmit = async (req, res) => {
-  const link = req.body.link;
+	const link = req.body.link;
 
-  try {
-    const browser = await puppeteer.launch({ headless: false, args: ["--start-maximized"] });
-    const page = await browser.newPage();
+	try {
+		const browser = await puppeteer.launch({
+			headless: false,
+			args: ['--start-maximized'],
+		});
+		const page = await browser.newPage();
 
-    await measureExecutionTime(async () => {
-      // Navigate the page to a URL
-      await page.goto("https://developer.chrome.com/");
+		await measureExecutionTime(async () => {
+			// Navigate the page to a URL
+			await page.goto(link);
 
-      // Set screen size
-      await page.setViewport({ width: 1920, height: 1080 });
+			// Set screen size
+			await page.setViewport({ width: 1920, height: 1080 });
 
-      // Type into search box
-      await page.type(".devsite-search-field", "automate beyond recorder");
+			// // Type into search box
+			// await page.type('.devsite-search-field', 'automate beyond recorder');
 
-      // Wait and click on first result
-      const searchResultSelector = ".devsite-result-item-link";
-      await page.waitForSelector(searchResultSelector);
-      await page.click(searchResultSelector);
+			// // Wait and click on first result
+			// const searchResultSelector = '.devsite-result-item-link';
+			// await page.waitForSelector(searchResultSelector);
+			// await page.click(searchResultSelector);
 
-      // Locate the full title with a unique string
-      const textSelector = await page.waitForSelector("text/Customize and automate");
-      const fullTitle = await textSelector?.evaluate((el) => el.textContent);
+			// // Locate the full title with a unique string
+			// const textSelector = await page.waitForSelector(
+			// 	'text/Customize and automate'
+			// );
+			// const fullTitle = await textSelector?.evaluate((el) => el.textContent);
 
-      // Print the full title
-      console.log('The title of this blog post is "%s".', fullTitle);
+			// // Print the full title
+			// console.log('The title of this blog post is "%s".', fullTitle);
 
-      await browser.close();
-    });
-  } catch (error) {
-    console.error("An error occurred:", error);
-    await res.status(500).send("An error occurred. Please try again later.");
-  } finally {
-    await res.redirect("/");
-  }
+			// await browser.close();
+		});
+	} catch (error) {
+		console.error('An error occurred:', error);
+		await res.status(500).send('An error occurred. Please try again later.');
+	} finally {
+		await res.redirect('/');
+	}
 };
 
 export default handleSubmit;
--- src/utils/measureExecutionTime.js
+++ src/utils/measureExecutionTime.js
@@ -1,11 +1,11 @@
 const measureExecutionTime = async (callback) => {
-  const startTime = performance.now();
-  await callback();
-  const endTime = performance.now();
+	const startTime = performance.now();
+	await callback();
+	const endTime = performance.now();
 
-  const executionTimeMs = endTime - startTime;
-  const executionTimeSec = executionTimeMs / 1000;
-  console.log(">>> Execution time:", executionTimeSec, "seconds <<<");
+	const executionTimeMs = endTime - startTime;
+	const executionTimeSec = executionTimeMs / 1000;
+	console.log('>>> Execution time:', executionTimeSec, 'seconds <<<');
 };
 
 export default measureExecutionTime;
